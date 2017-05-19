ca3abd4c72bedef1a5edaa345c20498ff58760cd
Viswanatha Swamy <vishipayyallore@yahoo.com>
2017-05-20 02:14:22 +0530

    .

---
+++ CSharpCorner_20May2017/Basics/ArrayLib/ArrayDemo.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq;
+using static System.Console;
+
+namespace ArrayLib
+{
+    public class ArrayDemo 
+    {
+        public void Run()
+        {
+            var numberOfItems = int.Parse(ReadLine().Trim());
+            var arrayValues = ReadLine().Trim().Split(' ').Select(int.Parse).ToArray();
+            WriteLine(string.Join(" ", arrayValues.Reverse()));
+        }
+    }
+}
---
+++ CSharpCorner_20May2017/Basics/StringLib/ToggleString.cs
@@ -0,0 +1,32 @@
+﻿using static System.Console;
+
+namespace StringLib
+{
+    public class ToggleString 
+    {
+        #region Methods
+        public void Run()
+        {
+            var data = ReadLine();
+            var output = 0;
+            foreach (var currentChar in data)
+            {
+                if ((currentChar >= 65 && currentChar <= 90))
+                {
+                    output = currentChar + 32;
+                }
+                else if ((currentChar >= 97 && currentChar <= 122))
+                {
+                    output = currentChar - 32;
+                }
+                else 
+                {
+                    output = currentChar;
+                }
+                Write($"{(char)output}");
+            }
+        }
+        #endregion
+
+    }
+}
