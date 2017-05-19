78c33bdb7ca768613e12f8c5010a667978e9af0e
Viswanatha Swamy <vishipayyallore@yahoo.com>
2017-05-20 02:36:00 +0530

    ...

--- CSharpCorner_20May2017/Basics/App1/Program.cs
+++ CSharpCorner_20May2017/Basics/App1/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using static System.Console;
+using System.Reflection;
 
 namespace App1
 {
@@ -6,7 +7,27 @@ namespace App1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var assemblyNames = new string[] { "ArrayLib", "StringLib" };
+            const string METHODNAME = "Run";
+
+            foreach (var assemblyName in assemblyNames)
+            {
+                RunPrograms(assemblyName, METHODNAME);
+            }
+
+            WriteLine("\n\nPress any key ...");
+            ReadKey();
+        }
+
+         private static void RunPrograms(string assemblyName, string methodName)
+        {
+            var programsAssembly = Assembly.Load(new AssemblyName(assemblyName));
+            foreach (var currentClass in programsAssembly.GetTypes())
+            {
+                var currentMethod = currentClass.GetMethod(methodName);
+                WriteLine($"{currentClass.Name} ....");
+                currentMethod.Invoke(System.Activator.CreateInstance(currentClass), null);
+            }
         }
     }
 }
