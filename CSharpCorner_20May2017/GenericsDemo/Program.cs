e837d717a493cb19a67fd9c8ea80ab4d13608814
monocept <monocept@monocepts-MacBook-Pro-2.local>
2017-06-02 11:23:37 +0530

    Modified in Mac OS

--- CSharpCorner_20May2017/GenericsDemo/Program.cs
+++ CSharpCorner_20May2017/GenericsDemo/Program.cs
@@ -2,12 +2,16 @@
 
 namespace GenericsDemo
 {
+    /// <summary>
+    /// Same code works in Ubuntu 14.04 and Windows 10. 
+    /// It also works in Mac OS!!!!
+    /// </summary>
     class Program
     {
         static void Main(string[] args)
         {
-            var numbers = new int[] {1, 2, 3, 4, 5 };
-            var vowels = new char[] {'A', 'B', 'C'};
+            var numbers = new int[] { 1, 2, 3, 4, 5 };
+            var vowels = new char[] { 'A', 'B', 'C' };
             var names = new string[] { "Shiva", "Mathews", "Azim" };
 
             var arrayPrinter = new ArrayPrinter();
