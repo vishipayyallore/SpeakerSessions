1a7d963ff3e2b2bb471eedea2c1bc0c19111fbe3
Viswanatha Swamy <vishipayyallore@yahoo.com>
2017-06-03 13:52:42 +0530

    Completed the demo on Windows 10

--- CSharpCorner_20May2017/MultiLibraryApp/ArrayLibrary/ArrayDemo.cs
+++ CSharpCorner_20May2017/MultiLibraryApp/ArrayLibrary/ArrayDemo.cs
@@ -1,14 +1,14 @@
 ﻿using System.Linq;
+using CoreLibrary;
 using static System.Console;
 
 namespace ArrayLibrary
 {
-    public class ArrayDemo : IProgram
+    public class ArrayDemo : IRunner
     {
         public void Run()
         {
             WriteLine("Sample Input {5 4 3 2 1}");
-            var numberOfItems = int.Parse(ReadLine().Trim());
             var arrayValues = ReadLine().Trim().Split(' ').Select(int.Parse).ToArray();
             WriteLine(string.Join(" ", arrayValues.Reverse()));
         }
--- CSharpCorner_20May2017/MultiLibraryApp/LogicPrograms/Program.cs
+++ CSharpCorner_20May2017/MultiLibraryApp/LogicPrograms/Program.cs
@@ -1,12 +1,49 @@
-﻿using System;
+﻿using static System.Console;
+using System.Reflection;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Json;
+using DataCore;
 
 namespace LogicPrograms
 {
+    /// <summary>
+    /// Project created in Windows 10. 
+    /// </summary>
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var metadata = GetMetadata("appsettings.json");
+
+            foreach (var current in metadata)
+            {
+                RunPrograms(current.AssemblyName, current.MethodName);
+            }
+
+
+            WriteLine("\n\nPress any key ...");
+            ReadKey();
         }
+
+        #region Private Methods.
+        private static void RunPrograms(string assemblyName, string methodName)
+        {
+            var programsAssembly = Assembly.Load(new AssemblyName(assemblyName));
+            foreach (var currentClass in programsAssembly.GetTypes())
+            {
+                var currentMethod = currentClass.GetMethod(methodName);
+                WriteLine($"{currentClass.Name} ....");
+                currentMethod.Invoke(System.Activator.CreateInstance(currentClass), null);
+            }
+        }
+
+        static List<Metadata> GetMetadata(string metadataFilePath)
+        {
+            var metadataFileStream = File.Open(metadataFilePath, FileMode.Open);
+            var serializer = new DataContractJsonSerializer(typeof(List<Metadata>));
+            return (List<Metadata>)serializer.ReadObject(metadataFileStream);
+        }
+        #endregion
     }
 }
