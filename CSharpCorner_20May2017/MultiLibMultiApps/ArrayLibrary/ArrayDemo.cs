7645ad62d2a1a4f08412a3811e255b3b724c3b15
monocept <monocept@monocepts-MacBook-Pro-2.local>
2017-06-04 12:40:18 +0530

    Initial Version from Mac OS X

---
+++ CSharpCorner_20May2017/MultiLibMultiApps/ArrayLibrary/ArrayDemo.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq;
+using CoreLibrary;
+using static System.Console;
+
+namespace ArrayLibrary
+{
+    public class ArrayDemo : IRunner
+    {
+        public void Run()
+        {
+            WriteLine("Sample Input {5 4 3 2 1}");
+            var arrayValues = ReadLine().Trim().Split(' ').Select(int.Parse).ToArray();
+            WriteLine(string.Join(" ", arrayValues.Reverse()));
+        }
+    }
+}
