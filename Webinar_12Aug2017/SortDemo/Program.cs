c889582826e45901d0e7b950fb0597953f8b1b06
Viswanatha Swamy <vishipayyallore@yahoo.com>
2017-08-12 17:59:42 +0530

    Initial Version from Ubuntu

---
+++ Webinar_12Aug2017/SortDemo/Program.cs
@@ -0,0 +1,49 @@
+﻿using System.Linq;
+using static System.Console;
+
+namespace SortDemo
+{
+    class Program
+    {
+	/// <summary>
+        /// Same code works in Ubuntu 14.04 and Windows 10. 
+        /// It also works in Mac OS!!!!
+        /// </summary>
+        static void Main(string[] args)
+        {
+            WriteLine("Enter list of number separated with space Ex: {1 2 3 4 5}:");
+	    var numbers = ReadLine().Trim().Split(' ').Select(int.Parse).ToArray();
+            var currentValue = 0;
+            var totalSwaps = 0;
+			
+	    for (int i = 0; i < numbers.Length; i++)
+            {
+                int numberOfSwaps = 0;
+
+                for (int j = 0; j < numbers.Length - 1; j++)
+                {
+                    if (numbers[j] > numbers[j + 1])
+                    {
+                        currentValue = numbers[j];
+                        numbers[j] = numbers[j + 1];
+                        numbers[j + 1] = currentValue;
+                        numberOfSwaps++;
+                    }
+                }
+
+                totalSwaps += numberOfSwaps;
+                if (numberOfSwaps == 0)
+                {
+                    break;
+                }
+            }
+
+            WriteLine($"Array is sorted in {totalSwaps} swaps.");
+            WriteLine($"First Element: {numbers.First()}");
+	    WriteLine($"Last Element: {numbers.Last()}");
+
+	    WriteLine("\n\nPress any key ...");
+	    ReadKey();
+        }
+    }
+}
